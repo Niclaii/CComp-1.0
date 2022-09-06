69c7e9a2cc17e776f8ad525686b43e7e8d77ea68
Niclaii <nicolas.quintanilla@ucsp.edu.pe>
2022-09-06 08:29:34 -0500

    edit

---
+++ Semana 2/Class.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+
+class Account{
+    int Cuenta{0};
+    Account(int nombreCuenta,int retiro)
+    :Cuenta{nombreCuenta}{
+        if (nombreCuenta>0){
+            nombreCuenta = true;}
+        else
+        {
+            nombreCuenta = false;
+        }
+        }
+
+    void retiro(int RetirarCantidad){
+        if (RetirarCantidad > Cuenta){
+            cout << "No cuenta con saldo suficiente para retirar" << endl;
+            }
+        
+        else{
+            Cuenta - RetirarCantidad;
+            } 
+        };
+}
---
+++ Semana 2/Ejercicio3.9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "Class.h"
+using namespace std;
+
+int main(){
+Account cuenta1{1000};
+
+cout << "introduzca el monto a retirar: " << endl;
+cin >> Account.retiro;
+
+cout <<  Account << endl;
+
+}  

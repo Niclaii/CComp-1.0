5b02516a45cb6cbcce69cf6c2d091448e1964157
ValenSaZu <camila.salazar@ucsp.edu.pe>
2022-10-18 07:40:41 -0500

    Punteros

---
+++ CComp2-1-Nicolas_Quintanilla-main/Semana 6/Contador.cpp
@@ -0,0 +1,21 @@
+int* ptr, contador; // así se declara un puntero.
+//Ejemplo
+int y{ 5 };
+int* yPtr(nullPtr) // -> se declara el puntero.
+yPtr = &y // asigna la direccion de y a yPtr
+
+
+/*
+Cuando tiene nullptr "apunta a nada" es nulo. Antes se ponía que era NULL o = a 0.
+Cuando en el puntero se ve * en un puntero es para declararlo.
+
+
+
+Operador &: Operador de dirección. (unario) Obtiene la direccion de memoria de la variable.
+
+
+
+
+
+
+*/
